46113dddaafa788b7f3e49de90c06a0e3c5d75ac
Vadym <vashchukvd@gmail.com>
2023-04-12 14:37:28 +0300

    first part done

--- Task_1/albums/albums.js
+++ Task_1/albums/albums.js
@@ -13,7 +13,7 @@ let mainRes = [];
 
 mainTitle.innerHTML = "All " + userName + "'s Albums";
 
-fetch("https://jsonplaceholder.typicode.com/albums")
+fetch("https://jsonplaceholder.typicode.com/albums") //получаю альбоми, які малюються в showAlbums()
   .then((response) => response.json())
   .then((res) => res.filter((cur) => cur.userId === userId))
   .then(function (resp) {
@@ -22,7 +22,7 @@ fetch("https://jsonplaceholder.typicode.com/albums")
     showAlbums(0);
   });
 
-function addPagination() {
+function addPagination() {            // функція для отрисовки кнопочок
   numberOfItems = mainRes.length;
   const pagesNeeded = Math.ceil(numberOfItems / albumsOnPage);
   for (i = 0; i < pagesNeeded; i++) {
@@ -32,22 +32,22 @@ function addPagination() {
   }
 }
 
-function showAlbums(pageNumer) {
+function showAlbums(pageNumer) {          //функція, яка робить самі альбоми при надатії на кнопку
   let activeBtn = document.querySelector(".pagination__btn.active");
   if (activeBtn) {
-    activeBtn.classList.remove("active");
+    activeBtn.classList.remove("active");      //це щоб прибрати актів з кнопочок
   }
 
   let currentBtn = document.querySelector(`#pagBtn${pageNumer}`);
-  currentBtn.classList.add("active");
+  currentBtn.classList.add("active");                //це щоб додавався актів при натисканні на кнопку
 
-  let start = pageNumer * albumsOnPage;
+  let start = pageNumer * albumsOnPage;         //це вираховується розмір слайса і робиться сам слайс основного масіва з альбомами
   let end = start + albumsOnPage;
   let notes = mainRes.slice(start, end);
 
   usersAlbums.innerHTML = "";
 
-  for (i = 0; i < notes.length; i++) {
+  for (i = 0; i < notes.length; i++) {              //тут просто через цикл слайс масиву додається в html
     usersAlbums.innerHTML += `<tr class="album__element">
           <td class="album__number">${mainRes.indexOf(notes[i]) + 1}</td>
           <td>${notes[i].title}</td>
@@ -55,11 +55,10 @@ function showAlbums(pageNumer) {
             notes[i].id
           })" id=${notes[i].id}>SHOW PHOTOS</button></td></tr>`;
   }
-  if (idOfTheShownAlbum != null) {
+  if (idOfTheShownAlbum != null) {                // це щоб актів на кнопках "показати фото" працював
     let shownAlbumBtn = document.getElementById(idOfTheShownAlbum);
-    if (shownAlbumBtn) shownAlbumBtn.classList.add("active");
+    if (shownAlbumBtn) {
+      shownAlbumBtn.classList.add("active");
+    }
   }
 }
-
-// ! далі щоб при нажатії на фотку вона теж більша ставала
-//! в кінці може зробити стрелочки прокрутки сторінок
